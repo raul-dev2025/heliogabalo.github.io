cda8feebe0d81eded52d2c6d29b573357945de8d
heliogabalo santos <jonitjuego@gmail.com>
2015-08-10 19:04:05 +0200

    config jquery

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,12 +1,3 @@
-$(document).ready(function() {
-   $('.album img').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('.album img').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-});
+window.onload = function () {
+   alert("cargado...");
+}
