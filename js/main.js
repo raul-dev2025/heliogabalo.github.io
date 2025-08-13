3da1f0feea9370b81e7470ea14dc36d7692cdf36
Raul Vilchez <r4u1974@gmail.com>
2025-08-13 18:47:29 +0200

    Add a click handler for the 'Nuestros Servicios' dropdown-toggle button to toggle its dropdown menu on mobile.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('.nav-tabs a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+  // Fix for custom dropdown in "Nuestros Servicios" for mobile
+  $('.btn-group .dropdown-toggle').on('click', function(e) {
+    e.preventDefault();
+    $(this).next('.dropdown-menu').toggle();
+  });
+
+  // Optional: Close dropdown when clicking outside
+  $(document).on('click', function(e) {
+    if (!$(e.target).closest('.btn-group').length) {
+      $('.btn-group .dropdown-menu').hide();
+    }
+  });
+});
