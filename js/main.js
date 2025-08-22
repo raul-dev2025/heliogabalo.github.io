633b85ffe5cb7e09a250d026088f1ca1980a1673
Raúl Vílchez <raulmicrosistemas@gmail.com>
2025-08-22 15:41:20 +0200

    Commented the code added 'blindily' to solve the problem whith the button 'Nuestros Servicios' in small devices.

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ $(function() {
     $(this).tab('show');
   });
 
-  // Fix for custom dropdown in "Nuestros Servicios" for mobile
-  $('.btn-group .dropdown-toggle').on('click', function(e) {
-    //e.preventDefault();
-    $(this).next('.dropdown-menu').toggle();
-  });
+  // // Fix for custom dropdown in "Nuestros Servicios" for mobile
+  // $('.btn-group .dropdown-toggle').on('click', function(e) {
+  //   //e.preventDefault();
+  //   $(this).next('.dropdown-menu').toggle();
+  // });
 
   // Optional: Close dropdown when clicking outside
-  $(document).on('click', function(e) {
-    if (!$(e.target).closest('.btn-group').length) {
-      $('.btn-group .dropdown-menu').hide();
-    }
-  });
+  // $(document).on('click', function(e) {
+  //   if (!$(e.target).closest('.btn-group').length) {
+  //     $('.btn-group .dropdown-menu').hide();
+  //   }
+  // });
 });
