0b036f1128bf2c71f269ad7cdb5b0d2187407a9d
Raúl Vílchez <raulmicrosistemas@gmail.com>
2025-08-27 16:08:45 +0200

    Fixed inconsistent indentation in app.controller function definition(line 4).

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-   app.controller('StoreController', ['$http', function($http) {
+  app.controller('StoreController', ['$http', function($http) {
     var store = this;
     store.products = [];
     $http.get('/js/store-products.json').success(function(data){
